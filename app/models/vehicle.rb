048a8ebea6033b3ec5a9f63c767c9f0e9ea9c12e
GroovyWizard <lucasfelipe260702@gmail.com>
2022-06-14 10:23:57 -0300

    Added search to vehicle, added translation for data tables

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,11 +1,20 @@
 class Vehicle < ApplicationRecord
-    belongs_to :client
-    has_many :services, dependent: :destroy
-    scope :from_this_month, lambda { 
-        where("vehicles.created_at > ? AND vehicles.created_at < ?", 
-        Time.now.beginning_of_month, Time.now.end_of_month) }
-    
-    validates :model, presence: true
-    validates :registration_plate, presence: true, uniqueness: true
-    
+  belongs_to :client
+  has_many :services, dependent: :destroy
+  scope :from_this_month, lambda {
+    where("vehicles.created_at > ? AND vehicles.created_at < ?",
+          Time.now.beginning_of_month, Time.now.end_of_month)
+  }
+
+  validates :model, presence: true
+  validates :registration_plate, presence: true, uniqueness: true
+
+  def self.search(search)
+    if search
+      @result = Vehicle.where("registration_plate LIKE ?", "%" + search + "%")
+      return @result ? @result : Vehicle.all
+    else
+      return Vehicle.all
+    end
+  end
 end
